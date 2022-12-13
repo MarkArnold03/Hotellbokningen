eb19c49e503d1bf2261325a10405fcbb36a77375
Mark Arnold Walusimbi <markanorld0@gmail.com>
2022-12-13 13:18:07 +0100

    #3 Added Room

---
+++ Hotellbokningen/Data/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotellbokningen.Data
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+    }
+}
